cd78b96af5a2a6a4b69f2333b07f7e18be42a472
Jesper vd Tol <jvandertol@student.scalda.nl>
2024-10-07 10:32:30 +0200

    final changes

--- src/NewPage.jsx
+++ src/NewPage.jsx
@@ -1,14 +1,7 @@
-import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 import StyledButton from './components/StyledButton';
-import ProjectElement from './components/ProjectElement';
 import ProjectImage from './components/ProjectImage';
-import AboutElement from './components/AboutElement';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPaperPlane } from '@fortawesome/free-regular-svg-icons'
-import { faUser, faPersonHiking, faMedal } from '@fortawesome/free-solid-svg-icons'
-import { faGithubSquare, faInstagramSquare, faLinkedin, faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
 import data from './data.json';
 
 function NewPage({ setCurrentPage, currentPage }) {
@@ -17,7 +10,7 @@ function NewPage({ setCurrentPage, currentPage }) {
 
   return (
     <>
-    <div class="blob-container"></div>
+    <div class="background-container"></div>
       <div id='newPage' className="d-flex flex-column align-items-center justify-content-center gap-3 w-50 mx-auto px-5">
         <StyledButton text="Back" className="position-absolute top-0 start-0 px-5 m-5" onClick={() => setCurrentPage("Home")} />
         {currentPage === "Skills" && (
